6b5dd49fbf2cde89fbd7786432bc1b3100e35d04
iamsourabhh <iamsourabhh@gmail.com>
2018-05-20 12:08:41 +0530

    Intial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import FrontPage from './pages/frontPage';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <FrontPage/>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
---
+++ src/pages/frontPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const FrontPage = () => (
+    <React.Fragment>
+        <h1>
+            Batman is spying on ya!!
+        </h1>
+    </React.Fragment>
+);
+export default FrontPage;
