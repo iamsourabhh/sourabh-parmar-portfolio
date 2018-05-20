4e7eb689dc5440aedfd3613084f6eb8a276e16e3
iamsourabhh <iamsourabhh@gmail.com>
2018-05-20 14:18:44 +0530

    completed phase I

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import FrontPage from './pages/frontPage';
+import SnowStorm from 'react-snowstorm';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
-        <FrontPage/>
+        <div style={{ backgroundColor: 'teal', height: '100vh', width: 'auto' }}>
+          <SnowStorm excludeMobile={false} />
+          <FrontPage />
+        </div>
       </React.Fragment>
     );
   }
--- src/pages/frontPage.js
+++ src/pages/frontPage.js
@@ -1,10 +1,37 @@
 import React from 'react';
+import { Icon } from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
 
 const FrontPage = () => (
     <React.Fragment>
-        <h1>
-            Batman is spying on ya!!
-        </h1>
+        <div style={{ position: '   ' }}>
+            <h1 style={{ position: 'absolute', color: '#fff', textAlign: 'center', marginLeft: '15em', marginTop: '5em' }}>
+                Hello, I'm Sourabh!
+                <br />
+                <small>
+                    Full Stack Ninja | Open Source Enthusiast | Blogger
+                </small>
+                <br />
+
+                <a target="_blank" href="https://github.com/iamsourabhh">
+                    <Icon inverted name='github' size='large' />
+                </a>
+                <a target="_blank" href="https://www.linkedin.com/in/iamsourabhh/">
+                    <Icon inverted name='linkedin' size='large' />
+                </a>
+                <a target="_blank" href="https://twitter.com/iamsourabhh">
+                    <Icon inverted name='twitter' size='large' />
+                </a>
+                <a target="_blank" href="https://www.facebook.com/sourabh.parmar.35">
+                    <Icon inverted name='facebook official' size='large' />
+                </a>
+                <a target="_blank" href="https://www.instagram.com/iamsourabhh/">
+                    <Icon inverted name='instagram' size='large' />
+                </a>
+
+            </h1>
+
+        </div>
     </React.Fragment>
 );
 export default FrontPage;
